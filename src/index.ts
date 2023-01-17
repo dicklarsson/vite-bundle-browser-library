99de73bf7b49a53686343056ce6b3d782cf97969
Dick Larsson <dicklarsson@gmail.com>
2023-01-17 21:03:03 +0100

    Enhance the index.ts example code

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
 import axios from "axios";
-/*
-
-    A module that uses external library (axios) and requests data from mock/fake online service
+/* 
+    A module that uses an external library (axios) and requests data from mock/fake online service
 */
 export async function getUsers() {
+
     const URL = "https://reqres.in/api/users?page=2";
     const axiosResponse = await axios.get(URL);
-    const apiData = axiosResponse.data;
 
-    console.log("Object values", Object.values(apiData.data));
-    return Object.values(apiData.data);
+    const apiResult = axiosResponse.data;
+    return Object.values(apiResult.data);
 }
 
